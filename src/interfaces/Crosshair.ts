5cb9a9633f5289a7beed145396191ee6d18645ba
Gordon <therealfantasia@gmx.de>
2021-12-10 13:36:11 +0100

    typescript

---
+++ src/interfaces/Crosshair.ts
@@ -0,0 +1,17 @@
+interface Crosshair {
+  gap: number,
+  outlineThickness: number,
+  r: number,
+  g: number,
+  b: number,
+  alpha: number,
+  outline: boolean,
+  color: number,
+  thickness: number,
+  t: boolean,
+  useAlpha: boolean,
+  useWeaponValue: boolean,
+  dot: boolean,
+  style: number,
+  size: number,
+}
